54c408872caef10dbbbcab2452f336f35b6b4578
Fran Parra <franparra@MacBook-Air-de-Fran.local>
2025-11-10 14:18:28 +0100

    porfa

--- Docker/Caronte/dockerfiles/base/ubuntu/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/ubuntu/admin/start.sh
@@ -1,1 +1,12 @@
-tail -f /dev/null
+newUser(){
+    useradd -rm -d /home/fran -s /bin/bash morgado
+    echo "fran:12345" | chpasswd
+    echo "Bienvenido fran" > /home/fran/bienvenida.txt
+}
+
+main(){
+    newUser()
+    tail -f /dev/null
+}
+
+main
